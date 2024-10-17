79b2df58d907a7962da528bd6830e770346a433f
Jingjie Li <jingjie.li.21@ucl.ac.uk>
2024-10-18 00:14:29 +0100

    handeling manual curation situation

--- +bc/+load/loadEphysData.m
+++ +bc/+load/loadEphysData.m
@@ -26,15 +26,36 @@ function [spikeTimes_samples, spikeTemplates, templateWaveforms, templateAmplitu
 %
 
 % load spike templates (= waveforms)
-if exist(fullfile([ephys_path filesep 'spike_clusters.npy']))
-  spike_templates_0idx = readNPY([ephys_path filesep 'spike_clusters.npy']); % already manually-curated / Ks4-n KS4, "spike_templates" is called "spike_clusters"
+if exist(fullfile([ephys_path filesep 'spike_clusters.npy']),'file')
+   spike_cluster = readNPY([ephys_path filesep 'spike_clusters.npy'])+1; % already manually-curated / Ks4-n KS4, "spike_templates" is called "spike_clusters"
+   spikeTemplates = readNPY([ephys_path filesep 'spike_templates.npy'])+1;
+   if max(spike_cluster) >= max(spikeTemplates)
+       % user did some manual curation cluster split on this recording
+       % session, so n_clusters is larger than n_templates
+       %
+       % to solve this, we need to see how many new clusters were generated
+       % first, and then for each new cluster, we need to go back and see
+       % which old cluster can be matched. Jingjie Li
+       spike_cluster_uq = unique(spike_cluster);
+       spikeTemplates_uq = unique(spikeTemplates);
+       new_cluster_index = setdiff(spike_cluster_uq, spikeTemplates_uq);
+    
+       new_cluster_matched_template_index = 0*new_cluster_index;
+       for ii = 1:numel(new_cluster_index)
+           item_index = find(spike_cluster==new_cluster_index(ii));
+           new_cluster_matched_template_index(ii) = spikeTemplates(item_index(1));
+       end
+       spikeTemplates_uq = [spikeTemplates_uq;new_cluster_matched_template_index];
+   end
+   spikeTemplates = spike_cluster;
 else 
   spike_templates_0idx = readNPY([ephys_path filesep 'spike_templates.npy']); 
-end
-spikeTemplates = spike_templates_0idx + 1;
+  spikeTemplates = spike_templates_0idx + 1;
+ end
+
 
 % load spike times 
-if exist(fullfile(ephys_path,'spike_times_corrected.npy')) % When running pyKS stitched you need the 'aligned / corrected' spike times
+if exist(fullfile(ephys_path,'spike_times_corrected.npy'),'file') % When running pyKS stitched you need the 'aligned / corrected' spike times
     spikeTimes_samples = double(readNPY([ephys_path filesep  'spike_times_corrected.npy']));
     spikeTimes_datasets = double(readNPY([ephys_path filesep  'spike_datasets.npy'])) + 1; %  which dataset? (zero-indexed so +1)
 else
@@ -47,6 +68,9 @@ templateAmplitudes = double(readNPY([ephys_path filesep 'amplitudes.npy'])); % e
 
 % Load and unwhiten templates
 templateWaveforms_whitened = readNPY([ephys_path filesep 'templates.npy']);
+if exist('spikeTemplates_uq','var')
+    templateWaveforms_whitened = templateWaveforms_whitened(spikeTemplates_uq, :, :);
+end
 winv = readNPY([ephys_path filesep 'whitening_mat_inv.npy']);
 templateWaveforms = zeros(size(templateWaveforms_whitened));
 for t = 1:size(templateWaveforms,1)
@@ -56,6 +80,9 @@ end
 if exist(fullfile([ephys_path filesep  'pc_features.npy']))
     pcFeatures = readNPY([ephys_path filesep  'pc_features.npy']);
     pcFeatureIdx = readNPY([ephys_path filesep  'pc_feature_ind.npy']) + 1;
+    if exist('spikeTemplates_uq','var')
+        pcFeatureIdx = pcFeatureIdx(spikeTemplates_uq, :);
+    end
 else  % not computed in early kilosort3 version - the distance and drift metrics (which are based on the PCs) will not be calculated 
     pcFeatures = NaN;
     pcFeatureIdx = NaN;
